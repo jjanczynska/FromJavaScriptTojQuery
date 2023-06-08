a0aae70e7654afeb0e73a82c905c2d7abe3cb4f1
jjanczynska <joaxjan@gmail.com>
2023-06-08 18:14:21 +0000

    update

--- 03-jQueryEvents/10-the_importance_of_this/red_and_black_boxes/js/script.js
+++ 03-jQueryEvents/10-the_importance_of_this/red_and_black_boxes/js/script.js
@@ -31,3 +31,11 @@ $(document).ready(function() {
     	}
     });
 })
+
+$('p').click(function(){
+	$(this).text("Changed only the text for this paragraph");
+});
+
+$('.card').mouseenter(function(){
+	$(this).fadeTo(2000, 0.2).fadeTo(2000, 1);
+});
